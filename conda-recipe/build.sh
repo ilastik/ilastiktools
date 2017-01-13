f88267461aaa78857530149429ea796222b2078a
Stuart Berg <bergs@janelia.hhmi.org>
2017-01-13 16:00:10 -0500

    conda-recipe: On Mac, use clang/libc++ instead of gcc/stdlibc++ (BTW, this means we disable OpenMP on Mac)

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,11 +5,19 @@ export LDFLAGS=""
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
     DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
+    WITH_OPENMP=OFF # As of Xcode 8.0, Apple's clang doesn't include openmp support.
+                    # We could install our own version of clang-omp via brew, but I'm not exploring that right now.
+    ILASTIKTOOLS_CXXFLAGS="${CFLAGS} -std=c++11 -stdlib=libc++"
 else
     DYLIB_EXT=so
-fi
+    CC=${PREFIX}/bin/gcc
+    CXX=${PREFIX}/bin/g++
+    WITH_OPENMP=ON
 
-ILASTIKTOOLS_CXXFLAGS="${CFLAGS} -std=c++11"
+    ILASTIKTOOLS_CXXFLAGS="${CFLAGS} -std=c++11"
+fi
 
 PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
 PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
@@ -18,8 +26,8 @@ PY_ABI=${PY_VER}${PY_ABIFLAGS}
 mkdir build
 cd build
 cmake ..\
-    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_CXX_FLAGS="${ILASTIKTOOLS_CXXFLAGS}" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
@@ -29,7 +37,7 @@ cmake ..\
     -DVIGRA_INCLUDE_DIR=${PREFIX}/include \
     -DVIGRA_IMPEX_LIBRARY=${PREFIX}/lib/libvigraimpex.${DYLIB_EXT} \
     -DVIGRA_NUMPY_CORE_LIBRARY=${SP_DIR}/vigra/vigranumpycore.so \
-    -DWITH_OPENMP=ON \
+    -DWITH_OPENMP=${WITH_OPENMP} \
 ##
 
 make -j${CPU_COUNT}
