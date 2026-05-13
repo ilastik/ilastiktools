11997dabe65800d0f33d1dbbee21dc584b617fe0
k-dominik <k-dominik@users.noreply.github.com>
2026-05-13 17:35:40 +0200

    Add regression/integration test from recipe

--- test-python/test_base.py
+++ test-python/test_base.py
@@ -1,38 +1,64 @@
 import ilastiktools
-import numpy as np 
+import numpy as np
+import numpy.typing as npt
+import pytest
 
-gridseg = ilastiktools.GridSegmentor_3D_UInt32()
 
-labels = np.arange(27).reshape((3, 3, 3)).astype(np.uint32)
-labels //= 10
-labels += 1
+@pytest.fixture
+def gridseg() -> ilastiktools.GridSegmentor_3D_UInt32:
+    return ilastiktools.GridSegmentor_3D_UInt32()
 
-print('labels', labels.dtype)
-feats = np.random.random((3, 3, 3)).astype(np.float32)
-print('feats', feats.dtype)
 
-gridseg.preprocessing(labels, feats)
-del labels
+@pytest.fixture
+def labels() -> npt.NDArray[np.uint32]:
+    labels = np.arange(27).reshape((3, 3, 3)).astype(np.uint32)
+    labels //= 10
+    labels += 1
+    return labels
 
-tmpout = gridseg.serializeGraph()
-print(tmpout)
-tmp = np.zeros(tmpout.shape, dtype=np.uint32)
-tmpout = gridseg.serializeGraph(tmp)
-print("same array values?", np.all(tmp == tmpout))
-print(tmp)
 
-brushStroke = np.zeros([3,3,3], dtype=np.uint8)
-brushStroke[1,1,1] = 1
-brushStroke[:,:,2] = 0
-brushStroke[:,:,0] = 0
-brushStroke[0,:,0] = 0
-gridseg.addSeeds(brushStroke=brushStroke,roiBegin=[0,0,0], 
-                            roiEnd=[3,3,3], maxValidLabel=2)
-gridseg.run(0.5, 0.01)
-seg = gridseg.getSegmentation([0,0,0], [3,3,1])
-print("Got segmentation of size ", seg.shape)
-print(seg)
-print('supervoxels', gridseg.getSuperVoxelSeg())
-print('SV seeds', gridseg.getSuperVoxelSeeds())
-seg = gridseg.getSegmentation([0,0,0], [3,3,1])
-seg = gridseg.getSegmentation([0,0,0], [3,3,1])
+@pytest.fixture
+def features() -> npt.NDArray[np.uint32]:
+    feats = np.random.random((3, 3, 3)).astype(np.float32)
+    return feats
+
+
+@pytest.fixture
+def brush_stroke() -> npt.NDArray[np.uint8]:
+    brush_stroke = np.zeros([3, 3, 3], dtype=np.uint8)
+    brush_stroke[1, 1, 1] = 1
+    brush_stroke[:, :, 2] = 0
+    brush_stroke[:, :, 0] = 0
+    brush_stroke[0, :, 0] = 0
+    return brush_stroke
+
+
+def test_minimal(
+    gridseg: ilastiktools.GridSegmentor_3D_UInt32,
+    labels: npt.NDArray[np.uint32],
+    features: npt.NDArray[np.uint32],
+    brush_stroke: npt.NDArray[np.uint8],
+):
+    gridseg.preprocessing(labels, features)
+    expected_graph = np.array([3, 2, 3, 1, 1, 2, 2, 3, 1, 1, 0, 2, 2, 2, 0, 1, 1, 3, 3, 1, 1, 2])
+    tmpout = gridseg.serializeGraph()
+    np.testing.assert_array_equal(tmpout, expected_graph)
+
+    tmp = np.zeros(tmpout.shape, dtype=np.uint32)
+    tmpout2 = gridseg.serializeGraph(tmp)
+    np.testing.assert_array_equal(tmpout2, tmp)
+
+    gridseg.addSeeds(brushStroke=brush_stroke, roiBegin=[0, 0, 0], roiEnd=[3, 3, 3], maxValidLabel=2)
+
+    gridseg.run(0.5, 0.01)
+    seg = gridseg.getSegmentation([0, 0, 0], [3, 3, 1])
+    expected_seg_shape = (3, 3, 1)
+    assert seg.shape == expected_seg_shape
+    np.testing.assert_array_equal(seg, np.ones_like(seg))
+    supervoxels = gridseg.getSuperVoxelSeg()
+    expected_supervoxels = np.array([0, 1, 1, 1])
+    np.testing.assert_array_equal(supervoxels, expected_supervoxels)
+
+    sv_seeds = gridseg.getSuperVoxelSeeds()
+    expected_sv_seeds = np.array([0, 0, 1, 0])
+    np.testing.assert_array_equal(sv_seeds, expected_sv_seeds)
