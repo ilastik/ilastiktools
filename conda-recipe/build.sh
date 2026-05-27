bcda3969b61557c1aad317ba2a5f335bc4ccb180
Dominik Kutra <k-dominik@users.noreply.github.com>
2026-05-27 22:09:20 +0200

    Enable openmp on macos (#20)
    llvm-openmp available via conda

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,9 +1,6 @@
 if [[ $(uname) == 'Darwin' ]]; then
-    WITH_OPENMP=OFF # As of Xcode 8.0, Apple's clang doesn't include openmp support.
-                    # We could install our own version of clang-omp via brew, but I'm not exploring that right now.
     ILASTIKTOOLS_CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
 else
-    WITH_OPENMP=ON
     ILASTIKTOOLS_CXXFLAGS="${CXXFLAGS} -std=c++11"
 fi
 
@@ -16,7 +13,7 @@ cmake ..\
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
     -DPython_EXECUTABLE=${PYTHON} \
-    -DWITH_OPENMP=${WITH_OPENMP} \
+    -DWITH_OPENMP=ON \
 ##
 
 make -j${CPU_COUNT}
